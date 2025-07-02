2419357774a8fc8756bc189f40eed29910c02408
Jelle De Loecker <jelle@elevenways.be>
2025-07-02 13:09:59 +0200

    👽️ Update ServerChunkLoadingManager mixin to 1.21.6

--- src/main/java/rocks/blackblock/bib/mixin/threading/ServerChunkLoadingManagerMixin.java
+++ src/main/java/rocks/blackblock/bib/mixin/threading/ServerChunkLoadingManagerMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.chunk.ChunkProvider;
 import net.minecraft.world.chunk.ChunkStatusChangeListener;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.level.storage.LevelStorage;
+import net.minecraft.server.world.ChunkTicketManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -27,7 +28,7 @@ public class ServerChunkLoadingManagerMixin {
     @Shadow @Final private ThreadExecutor<Runnable> mainThreadExecutor;
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void onInit(ServerWorld world, LevelStorage.Session session, DataFixer dataFixer, StructureTemplateManager structureTemplateManager, Executor executor, ThreadExecutor mainThreadExecutor, ChunkProvider chunkProvider, ChunkGenerator chunkGenerator, WorldGenerationProgressListener worldGenerationProgressListener, ChunkStatusChangeListener chunkStatusChangeListener, Supplier persistentStateManagerFactory, int viewDistance, boolean dsync, CallbackInfo ci) {
+    private void onInit(ServerWorld world, LevelStorage.Session session, DataFixer dataFixer, StructureTemplateManager structureTemplateManager, Executor executor, ThreadExecutor mainThreadExecutor, ChunkProvider chunkProvider, ChunkGenerator chunkGenerator, WorldGenerationProgressListener worldGenerationProgressListener, ChunkStatusChangeListener chunkStatusChangeListener, Supplier persistentStateManagerFactory, ChunkTicketManager chunkTicketManager, int viewDistance, boolean dsync, CallbackInfo ci) {
         BibFlow.registerWorldChunkExecutor(world, this.mainThreadExecutor);
     }
 }
